69728596e639d754b444016bd2d3a6a452b1c023
samukaii <samuelalejandro.dev@gmail.com>
2024-11-29 22:27:34 -0300

    Refactor: streamline orders-import component import declarations
    Simplified the imports in the orders-import component by removing unused ButtonComponent and FlexRowComponent. This change enhances maintainability without altering current behavior.

--- src/app/pages/orders/import/orders-import.component.ts
+++ src/app/pages/orders/import/orders-import.component.ts
@@ -1,15 +1,11 @@
 import { Component, output } from '@angular/core';
 import { createOrdersImportForm, ImportOrdersPayload } from "./create-orders-import.form";
-import { ButtonComponent } from "../../../shared/components/button/button.component";
-import { FlexRowComponent } from "../../../shared/components/flex-row/flex-row.component";
 import { FormComponent } from "../../../shared/components/form/form/form.component";
 import { FileUploadComponent } from "../../../shared/components/file-upload/file-upload.component";
 
 @Component({
     selector: 'app-orders-import',
 	imports: [
-		ButtonComponent,
-		FlexRowComponent,
 		FormComponent,
 		FileUploadComponent
 	],
