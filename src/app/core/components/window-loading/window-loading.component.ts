368b0a4724b043fe8c2fae7b1a6d2dbe69af50e4
samukaii <samuelalejandro.dev@gmail.com>
2024-12-01 06:49:20 -0300

    feature: Enhance loading behavior with preservation mode
    - Implement a 'no-preservation' mode for the window-loading component.
    - Add a mat-progress-bar for visual feedback during preservation loading.
    - Introduce logic to determine loading state and mode dynamically.

--- src/app/core/components/window-loading/window-loading.component.ts
+++ src/app/core/components/window-loading/window-loading.component.ts
@@ -1,16 +1,36 @@
-import { Component, input } from '@angular/core';
+import { booleanAttribute, Component, computed, input } from '@angular/core';
 import { ProgressSpinnerComponent } from "../progress-spinner/progress-spinner.component";
+import { MatProgressBar } from "@angular/material/progress-bar";
 
 @Component({
     selector: 'app-window-loading',
-    imports: [
-        ProgressSpinnerComponent
-    ],
+	imports: [
+		ProgressSpinnerComponent,
+		MatProgressBar
+	],
     templateUrl: './window-loading.component.html',
     styleUrl: './window-loading.component.scss'
 })
 export class WindowLoadingComponent {
 	loading = input(false);
+	noPreservation = input(false, {transform: booleanAttribute});
 	strokeWidth = input(4);
 	diameter = input(40);
+
+	firstLoad = true;
+
+	loadingMode = computed(() => {
+		if(!this.loading()) return 'no-loading';
+
+		if(this.firstLoad) {
+			this.firstLoad = false;
+			return 'no-preservation';
+		}
+
+		if(this.noPreservation()) return 'no-preservation';
+
+		if(!this.firstLoad) return 'preservation';
+
+		return 'no-preservation';
+	})
 }
