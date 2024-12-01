218d1b23b789a1f79a44b14609f4effddf7592ea
samukaii <samuelalejandro.dev@gmail.com>
2024-12-01 06:50:52 -0300

    feature: Add browser check and display average line in chart
    - Implement browser check to conditionally render the canvas.
    - Add option to display an average line on the line chart.
    - Modify point styling based on data point values.

--- src/app/shared/components/charts/line/line-chart.component.ts
+++ src/app/shared/components/charts/line/line-chart.component.ts
@@ -1,5 +1,7 @@
-import { Component, computed, effect, ElementRef, input, viewChild } from '@angular/core';
-import Chart from 'chart.js/auto';
+import { booleanAttribute, Component, computed, effect, ElementRef, input, viewChild } from '@angular/core';
+import Chart, { ScriptableContext } from 'chart.js/auto';
+import { injectIsAtBrowser } from "../../../di/inject-is-at-browser";
+import { ChartDataset } from "chart.js";
 
 interface ChartData {
 	label: string | Date;
@@ -32,18 +34,25 @@ const getColor = (index: number): string => {
 @Component({
 	selector: 'app-line-chart',
 	templateUrl: './line-chart.component.html',
+	imports: [],
 	styleUrl: './line-chart.component.scss'
 })
 export class LineChartComponent {
 	direction = input<"horizontal" | "vertical">("horizontal");
 	data = input.required<ChartData[]>();
 	label = input.required<string>();
+	showAverage = input(false, {transform: booleanAttribute});
+	isAtBrowser = false;
 
 	chartElement = viewChild('canvas', {read: ElementRef});
 
 	element = computed(() =>
 		this.chartElement()?.nativeElement as HTMLCanvasElement | undefined
 	);
+
+	constructor() {
+		this.isAtBrowser = injectIsAtBrowser();
+	}
 
 	chart?: Chart;
 
@@ -53,6 +62,10 @@ export class LineChartComponent {
 		const data = this.data();
 		const element = this.element();
 
+		const average = data.reduce((previousValue, currentValue) =>
+			previousValue + currentValue.value, 0
+		) / data.length;
+
 		if (!element) return;
 
 		this.chart = new Chart(element, {
@@ -60,14 +73,34 @@ export class LineChartComponent {
 			data: {
 				labels: this.data().map(item => item.label),
 				datasets: [
+					this.showAverage() ? {
+						label: "Média",
+						type: 'line',
+						data: data.map(() => average),
+						borderWidth: 1,
+						borderColor: '#005f99',
+						pointHitRadius: 30,
+						pointStyle: false,
+						backgroundColor: 'rgba(0,95,153,0.24)',
+					} as ChartDataset<'line'> : [],
 					{
 						label: this.label(),
 						data: data.map(item => item.value),
 						backgroundColor: '#1c2541',
 						borderWidth: 1,
-						borderColor: '#1c2541'
-					},
-				]
+						borderColor: '#1c2541',
+						pointBorderColor: (item: ScriptableContext<'line'>) => {
+							if (item.raw === 0) return 'transparent';
+
+							return 'red'
+						},
+						pointBackgroundColor: (item: ScriptableContext<'line'>) => {
+							if (item.raw === 0) return 'transparent';
+
+							return 'red'
+						},
+					}
+				].flat() as ChartDataset[]
 			},
 			options: {
 				indexAxis: this.direction() === "horizontal" ? "x" : "y",
