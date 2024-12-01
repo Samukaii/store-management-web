ae9399f14de64b6b0a09a6ca0a1ac23c3cfbfe21
samukaii <samuelalejandro.dev@gmail.com>
2024-12-01 06:50:17 -0300

    feature: Add new DatePickerComponent
    - Implement a new DatePickerComponent with Angular Material datepicker.
    - Introduce template and styling files for the component.
    - Include unit test for DatePickerComponent creation.

---
+++ src/app/shared/components/date-picker/date-picker.component.ts
@@ -0,0 +1,67 @@
+import { booleanAttribute, Component, input } from '@angular/core';
+import { MatDatepicker, MatDatepickerInput, MatDatepickerToggle } from "@angular/material/datepicker";
+import { MatFormField, MatLabel, MatSuffix } from "@angular/material/form-field";
+import { FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { MatInput } from "@angular/material/input";
+import { Moment } from "moment/moment";
+import { getControl } from "../chips-selector/chips-selector.component";
+import moment from "moment";
+
+@Component({
+    selector: 'app-datepicker',
+	imports: [
+		ReactiveFormsModule,
+		MatSuffix,
+		MatDatepickerToggle,
+		MatLabel,
+		MatFormField,
+		MatDatepicker,
+		MatInput,
+		MatDatepickerInput
+	],
+    templateUrl: './date-picker.component.html',
+    styleUrl: './date-picker.component.scss'
+})
+export class DatePickerComponent {
+	form = input.required<FormGroup>();
+	name = input.required<string>();
+	startView = input<"month" | "year" | "multi-year">("month");
+	closeOnYearSelect = input(false, {transform: booleanAttribute})
+	closeOnMonthSelect = input(false, {transform: booleanAttribute})
+
+	label = input("");
+
+	get control() {
+		return getControl(this.form(), this.name());
+	}
+
+
+	setMonthAndYear(normalizedMonthAndYear: Moment, datepicker: MatDatepicker<Moment>) {
+		if(!this.closeOnMonthSelect()) return;
+
+		let value = this.control.value ?? moment();
+
+		if(value instanceof Date)
+			value = moment(value);
+
+		value.month(normalizedMonthAndYear.month());
+		value.year(normalizedMonthAndYear.year());
+
+		this.control.setValue(value);
+		datepicker.close();
+	}
+
+	setYear(normalizedMonthAndYear: Moment, datepicker: MatDatepicker<Moment>) {
+		if(!this.closeOnYearSelect()) return;
+
+		let value = this.control.value ?? moment();
+
+		if(value instanceof Date)
+			value = moment(value);
+
+		value.year(normalizedMonthAndYear.year());
+
+		this.control.setValue(value);
+		datepicker.close();
+	}
+}
