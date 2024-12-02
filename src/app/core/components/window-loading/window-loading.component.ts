a77396f55f4d3e00fa222c73cacf9ff2b61a6649
samukaii <samuelalejandro.dev@gmail.com>
2024-12-02 18:05:44 -0300

    feature: Integrate TopBarLoadingService into window loading
    - Injected TopBarLoadingService into WindowLoadingComponent to manage top bar loading state.
    - Removed MatProgressBar integration and adjusted template accordingly.
    - Added an effect to update top bar loading based on the loadingMode state.

--- src/app/core/components/window-loading/window-loading.component.ts
+++ src/app/core/components/window-loading/window-loading.component.ts
@@ -1,23 +1,28 @@
-import { booleanAttribute, Component, computed, input } from '@angular/core';
+import { booleanAttribute, Component, computed, effect, inject, input } from '@angular/core';
 import { ProgressSpinnerComponent } from "../progress-spinner/progress-spinner.component";
-import { MatProgressBar } from "@angular/material/progress-bar";
+import { TopBarLoadingService } from "../top-bar/top-bar-loading.service";
 
 @Component({
     selector: 'app-window-loading',
 	imports: [
-		ProgressSpinnerComponent,
-		MatProgressBar
+		ProgressSpinnerComponent
 	],
     templateUrl: './window-loading.component.html',
     styleUrl: './window-loading.component.scss'
 })
 export class WindowLoadingComponent {
+	private service = inject(TopBarLoadingService);
+
 	loading = input(false);
 	noPreservation = input(false, {transform: booleanAttribute});
 	strokeWidth = input(4);
 	diameter = input(40);
 
 	firstLoad = true;
+
+	updateTopBarLoading = effect(() => {
+		this.service.setLoading(this.loadingMode() === "preservation");
+	});
 
 	loadingMode = computed(() => {
 		if(!this.loading()) return 'no-loading';
