1d02295a9d0c4607ac75c7a22a2a32a8a0fac759
samukaii <samuelalejandro.dev@gmail.com>
2024-12-03 14:08:11 -0300

    feature: Enhance order analytics component features
    - Updated import statements to include new dependencies.
    - Introduced `monthsSince` function to calculate months elapsed.
    - Added `displayOptions` for billing and sales quantity display.
    - Changed calculation approach for period and added average computations.
    - Updated methods for handling chart data with distinct billing and quantity types.
    - Implemented chart configurations for better visualization with objectives.

--- src/app/pages/analytics/analytics-orders/analytics-orders.component.ts
+++ src/app/pages/analytics/analytics-orders/analytics-orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, untracked } from '@angular/core';
 import { FlexRowComponent } from "../../../shared/components/flex-row/flex-row.component";
 import { LineChartComponent } from "../../../shared/components/charts/line/line-chart.component";
 import { AnalyticsService } from "../analytics.service";
@@ -14,10 +14,13 @@ import { BasicOption } from "../../../shared/models/basic-option";
 import { formValue } from "../../../shared/helpers/form-value";
 import { distinctPropertiesAvoidingNull } from "../../../shared/helpers/distinct-properties-avoiding-null";
 import { distinctValue } from "../../../shared/helpers/distinct-value";
-import { BarChartComponent } from "../../../shared/components/charts/bar/bar-chart.component";
+import { BarChartComponent, chartBluePalette } from "../../../shared/components/charts/bar/bar-chart.component";
 import { DatePickerComponent } from "../../../shared/components/date-picker/date-picker.component";
 import { YearFormatDirective } from "../../../shared/directives/year-format/year-format.directive";
 import { capitalize } from "../../../shared/helpers/capitalize";
+import { ChartData } from "chart.js/auto";
+import { ChartDataset, ScriptableContext } from "chart.js";
+import { OrdersPeriodType } from "../enum/orders-period-type";
 
 const fillPeriodWithDates = (start: Date | string, end: Date | string) => {
 	let current = new Date(start);
@@ -33,14 +36,42 @@ const fillPeriodWithDates = (start: Date | string, end: Date | string) => {
 
 const fillPeriodWithMonths = (start: Date | string, end: Date | string) => {
 	let current = new Date(start);
+	let last = new Date(end);
+
+	current.setDate(1);
+	current.setHours(12, 0,  0, 0);
+	last.setMonth(last.getMonth() + 1);
+	last.setDate(0);
+	last.setHours(12, 0,  0, 0);
+
 	const dates = [];
 
-	while (current.getMonth() <= new Date(end).getMonth() && current.getFullYear() <= new Date(end).getFullYear()) {
+	while (current.getTime() <= last.getTime()) {
 		dates.push(new Date(current));
 		current.setMonth(current.getMonth() + 1);
 	}
 
 	return dates;
+}
+
+const monthsSince = (date: Date | string) => {
+	const sinceDate = new Date(date);
+	const now = new Date();
+
+	sinceDate.setDate(15);
+	sinceDate.setHours(12, 0,   0, 0);
+
+	now.setDate(15);
+	now.setHours(12, 0,   0, 0);
+
+	let count = 0;
+
+	while (sinceDate.getTime() < now.getTime()) {
+		count++;
+		sinceDate.setMonth(sinceDate.getMonth() + 1);
+	}
+
+	return count;
 }
 
 @Component({
@@ -60,22 +91,34 @@ const fillPeriodWithMonths = (start: Date | string, end: Date | string) => {
 })
 export class AnalyticsOrdersComponent {
 	service = inject(AnalyticsService);
-	protected readonly OrdersDisplayType = OrdersDisplayType;
+	protected readonly OrdersPeriodType = OrdersPeriodType;
 
 	form = inject(FormBuilder).group({
-		periodType: [OrdersDisplayType.DAY],
+		periodType: [OrdersPeriodType.DAY],
+		displayType: [OrdersDisplayType.BILLING],
 		startDate: [extendedDate().minus(1, 'month').get()],
 		endDate: [extendedDate().get()],
 	});
 
+	displayOptions: BasicOption[] = [
+		{
+			name: "Faturamento",
+			value: OrdersDisplayType.BILLING
+		},
+		{
+			name: "Quantidade de vendas",
+			value: OrdersDisplayType.QUANTITY
+		}
+	];
+
 	options: BasicOption[] = [
 		{
 			name: "Dias",
-			value: OrdersDisplayType.DAY
+			value: OrdersPeriodType.DAY
 		},
 		{
 			name: "Meses",
-			value: OrdersDisplayType.MONTH
+			value: OrdersPeriodType.MONTH
 		}
 	];
 
@@ -83,7 +126,7 @@ export class AnalyticsOrdersComponent {
 	period = computed(() => {
 		const formValue = this.formValue();
 
-		if (formValue.periodType === OrdersDisplayType.DAY) {
+		if (formValue.periodType === OrdersPeriodType.DAY) {
 			return {
 				'date:min': formValue.startDate?.toISOString() ?? null,
 				'date:max': formValue.endDate?.toISOString() ?? null,
@@ -91,8 +134,8 @@ export class AnalyticsOrdersComponent {
 		}
 
 		return {
-			'date:min': formValue.startDate ? extendedDate(formValue.startDate).firstDayOf('year').getISO() : null,
-			'date:max': formValue.startDate ? extendedDate(formValue.startDate).lastDayOf('year').getISO() : null,
+			'date:min': formValue.startDate ? extendedDate(new Date("2024-07-01T12:00:00")).getISO() : null,
+			'date:max': formValue.startDate ? extendedDate(new Date("2024-07-01T12:00:00")).plus(12, 'month').getISO() : null,
 		}
 	}, {equal: distinctPropertiesAvoidingNull(['date:max', 'date:min'])});
 
@@ -108,11 +151,12 @@ export class AnalyticsOrdersComponent {
 		loader: ({request: params}) => this.service.ordersByPeriod(params)
 	});
 
-	ordersChartData = computed(() => {
+	ordersChartItems = computed(() => {
 		const orders = this.orders.value() ?? [];
+		const displayType = this.formValue().displayType;
 		let data: { label: string; value: number }[] = [];
 
-		if (this.filters().periodType === OrdersDisplayType.DAY) {
+		if (this.filters().periodType === OrdersPeriodType.DAY) {
 			const dates = fillPeriodWithDates(this.period()['date:min']!, this.period()['date:max']!);
 
 			data = dates.map(date => {
@@ -122,25 +166,29 @@ export class AnalyticsOrdersComponent {
 
 				return {
 					label: formatDate(date, 'dd/MM', 'pt'),
-					value: order?.total ?? 0
+					value: displayType === OrdersDisplayType.BILLING
+						? order?.total ?? 0
+						: order?.quantity ?? 0
 				}
 			})
 		}
 
-		if (this.filters().periodType === OrdersDisplayType.MONTH) {
+		if (this.filters().periodType === OrdersPeriodType.MONTH) {
 			const dates = fillPeriodWithMonths(
-				extendedDate().firstDayOf('year').get(),
-				extendedDate().lastDayOf('year').get()
+				extendedDate(new Date("2024-07-01T12:00:00")).get(),
+				extendedDate(new Date("2024-07-01T12:00:00")).plus(12, 'month').get()
 			);
 
 			data = dates.map(date => {
 				const order = orders.find(order =>
-					formatDate(order.date, 'MM', 'en') === formatDate(date, 'MM', 'pt')
+					formatDate(order.date, 'MM (YYYY)', 'en') === formatDate(date, 'MM (YYYY)', 'pt')
 				);
 
 				return {
-					label: capitalize(formatDate(date, 'MMMM', 'pt')),
-					value: order?.total ?? 0
+					label: capitalize(formatDate(date, 'MMMM/YYYY', 'pt')),
+					value: displayType === OrdersDisplayType.BILLING
+						? order?.total ?? 0
+						: order?.quantity ?? 0
 				}
 			})
 		}
@@ -148,14 +196,108 @@ export class AnalyticsOrdersComponent {
 		return data;
 	});
 
+	monthlyChartData = computed((): ChartData => {
+		const startDate = untracked(this.period)['date:min'];
+
+		const data = this.ordersChartItems();
+		const average = data.reduce((previousValue, currentValue) =>
+			previousValue + currentValue.value, 0
+		) / monthsSince(startDate!);
+
+		const displayType = this.formValue().displayType;
+
+		return {
+			labels: data.map(item => item.label),
+			datasets: [
+				{
+					label: "Objetivo mensal",
+					type: 'line',
+					data: data.map(() => displayType === OrdersDisplayType.BILLING ? 6000: 30 * 4),
+					borderWidth: 1,
+					borderColor: '#0f9360',
+					pointHitRadius: 30,
+					pointStyle: false,
+					backgroundColor: '#0f9360',
+				},
+				{
+					label: "Média mensal",
+					type: 'line',
+					data: data.map(() => displayType === OrdersDisplayType.BILLING ? average  : Math.round(average)),
+					borderWidth: 1,
+					borderColor: '#005f99',
+					pointStyle: false,
+					pointHitRadius: 30,
+					backgroundColor: 'rgba(0,95,153,0.24)',
+				},
+				{
+					label: displayType === OrdersDisplayType.BILLING ? "Faturamento": "Quantidade de vendas",
+					data: data.map(item => item.value),
+					backgroundColor: chartBluePalette,
+					borderColor: chartBluePalette
+				},
+			]
+		}
+	})
+
+	dailyChartData = computed((): ChartData<"line"> => {
+		const data = this.ordersChartItems();
+		const average = data.reduce((previousValue, currentValue) =>
+			previousValue + currentValue.value, 0
+		) / data.length;
+		const displayType = this.formValue().displayType;
+
+		return {
+			labels: data.map(item => item.label),
+			datasets: [
+				{
+					label: "Objetivo diário",
+					type: 'line',
+					data: data.map(() => displayType === OrdersDisplayType.BILLING ? 6000/30 : 4),
+					borderWidth: 1,
+					borderColor: '#0f9360',
+					pointHitRadius: 30,
+					pointStyle: false,
+					backgroundColor: '#0f9360',
+				},
+				{
+					label: "Média diária",
+					type: 'line',
+					data: data.map(() => displayType === OrdersDisplayType.BILLING ? average  : Math.round(average)),
+					borderWidth: 1,
+					borderColor: '#005f99',
+					pointHitRadius: 30,
+					pointStyle: false,
+					backgroundColor: 'rgba(0,95,153,0.24)',
+				},
+				{
+					label: displayType === OrdersDisplayType.BILLING ? "Faturamento": "Quantidade de vendas",
+					data: data.map(item => item.value),
+					backgroundColor: '#1c2541',
+					borderWidth: 1,
+					borderColor: '#1c2541',
+					pointBorderColor: (item: ScriptableContext<'line'>) => {
+						if (item.raw === 0) return 'transparent';
+
+						return 'red'
+					},
+					pointBackgroundColor: (item: ScriptableContext<'line'>) => {
+						if (item.raw === 0) return 'transparent';
+
+						return 'red'
+					},
+				},
+			]
+		}
+	});
+
 	totalBilling = computed(() => {
-		const data = this.ordersChartData().reverse();
+		const data = this.ordersChartItems().reverse();
 
 		return data.reduce((total, current) => total + current.value, 0);
 	});
 
 	billingAverage = computed(() => {
-		const data = this.ordersChartData().reverse();
+		const data = this.ordersChartItems().reverse();
 
 		return (this.totalBilling() ?? 0) / data.length;
 	});
