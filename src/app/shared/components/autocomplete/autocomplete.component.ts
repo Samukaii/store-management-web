ca293ac4eb8623211bac752817ff2b4896514b59
samukaii <samuelalejandro.dev@gmail.com>
2024-12-07 01:29:18 -0300

    feat(#349): Improve autocomplete component
    - Updated AutocompleteComponent to display a loading
      indicator while fetching data.
    - Added handling for empty results, displaying a "No
      results found" message.
    - Implemented error handling using FieldErrorComponent.
    - Added styles for loading state and list item width.
    - Refactored component logic to use signals and
      simplified data binding.
    - Fixed an issue where the search input would not
      update correctly when a value was selected.
    - Improved error handling and synchronization between
      the control and search input.
    - Added logic to search by ID when the control value
      changes.

--- src/app/shared/components/autocomplete/autocomplete.component.ts
+++ src/app/shared/components/autocomplete/autocomplete.component.ts
@@ -1,83 +1,144 @@
-import { Component, computed, DestroyRef, inject, input, OnInit, output } from '@angular/core';
+import { Component, DestroyRef, inject, input, OnInit, output, signal } from '@angular/core';
 import {
 	MatAutocomplete,
 	MatAutocompleteSelectedEvent,
 	MatAutocompleteTrigger,
 	MatOption
 } from "@angular/material/autocomplete";
-import { FormControl, FormGroup, NonNullableFormBuilder, ReactiveFormsModule } from "@angular/forms";
-import { MatFormField, MatInput, MatLabel } from "@angular/material/input";
+import { FormControl, NonNullableFormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { MatError, MatFormField, MatInput, MatLabel } from "@angular/material/input";
 import { rxResource, takeUntilDestroyed, toSignal } from "@angular/core/rxjs-interop";
 import { AutocompleteMethod } from "./models/autocomplete-method";
 import { AutocompleteOption } from "./models/autocomplete-option";
-import { map } from "rxjs";
+import { filter, map, of, startWith } from "rxjs";
 import { toNumberOrNull } from "../../helpers/to-number-or-null";
+import { ProgressSpinnerComponent } from "../../../core/components/progress-spinner/progress-spinner.component";
+import { FieldErrorComponent } from "../../directives/field-error.component";
 
 @Component({
     selector: 'app-autocomplete',
-    imports: [
-        MatAutocomplete,
-        MatOption,
-        ReactiveFormsModule,
-        MatAutocompleteTrigger,
-        MatInput,
-        MatLabel,
-        MatFormField
-    ],
+	imports: [
+		MatAutocomplete,
+		MatOption,
+		ReactiveFormsModule,
+		MatAutocompleteTrigger,
+		MatInput,
+		MatLabel,
+		MatFormField,
+		ProgressSpinnerComponent,
+		MatError,
+		FieldErrorComponent
+	],
     templateUrl: './autocomplete.component.html',
     styleUrl: './autocomplete.component.scss'
 })
 export class AutocompleteComponent implements OnInit {
-	form = input.required<FormGroup>();
-	name = input.required<string>();
+	control = input.required<FormControl>();
 	method = input.required<AutocompleteMethod>();
 	label = input("");
 	placeholder = input("");
 	select = output<AutocompleteOption>();
 
-	private destroyRef = inject(DestroyRef);
-
-	control = computed(() => {
-		return this.form().get(this.name()) as FormControl;
-	});
+	destroyRef = inject(DestroyRef);
 
 	internalForm = inject(NonNullableFormBuilder).group({
 		search: [""]
 	});
 
-	search = toSignal(this.internalForm.controls.search.valueChanges, {
-		initialValue: ""
-	});
+	search = signal("");
+	preventEmission = false;
 
 	filteredData = rxResource({
 		request: this.search,
-		loader: ({request: nameSearch}) => this.method()({'name:search': nameSearch})
+		loader: ({request: nameSearch}) => {
+
+			if (nameSearch)
+				return this.method()({'name:search': nameSearch});
+
+			return this.method()({});
+		}
 	});
 
 	onSelect($event: MatAutocompleteSelectedEvent) {
-		this.control().setValue($event.option.value.id);
-		this.internalForm.controls.search.setValue($event.option.value.name);
+		const value = $event.option.value as AutocompleteOption;
 
-		this.select.emit($event.option.value);
+		if (!value) return;
+
+		this.control().setValue(value.id);
+		this.internalForm.controls.search.setValue(value.name, {emitEvent: false});
+
+		this.select.emit(value);
 	}
 
 	ngOnInit() {
-		this.control().valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(value => {
+		this.syncErrors();
+		this.watchControl();
+		this.watchSearch();
+	}
+
+	private watchControl() {
+		const search = this.internalForm.controls.search;
+
+		const controlChanges$ = this.control().valueChanges.pipe(takeUntilDestroyed(this.destroyRef));
+
+		controlChanges$.subscribe(value => {
 			const id = toNumberOrNull(value);
 
 			if(id !== null) {
-				this.getValue(id).subscribe(result => {
-					this.internalForm.controls.search.setValue(result?.name ?? "", {emitEvent: false});
+				this.searchId(id).subscribe(result => {
+					search.setValue(result?.name ?? "", {emitEvent: false});
 				});
 
 				return;
 			}
 
-			this.internalForm.controls.search.setValue(value, {emitEvent: false});
-		})
+			search.setValue(value, {emitEvent: false});
+		});
 	}
 
-	getValue(id: number) {
-		return this.method()({'id:equal': id}).pipe(map(items => items.at(0)))
+	private syncErrors() {
+		const search = this.internalForm.controls.search;
+
+		const controlError = this.getControlError$();
+
+		controlError.subscribe(error => {
+			search.setValidators(() => error);
+			this.preventEmission = true;
+			search.updateValueAndValidity();
+		});
+	}
+
+	private getControlError$() {
+		return this.control().statusChanges.pipe(
+			map(() => this.control().errors),
+			startWith(this.control().errors),
+			takeUntilDestroyed(this.destroyRef)
+		)
+	}
+
+	private searchId(id: number) {
+		const foundOption = this.filteredData.value()?.find(option => option.id === id);
+
+		if (foundOption) return of(foundOption);
+
+		return this.method()({'id:equal': id}).pipe(
+			map(items => items.at(0)),
+			takeUntilDestroyed(this.destroyRef)
+		)
+	}
+
+	private watchSearch() {
+		const search = this.internalForm.controls.search;
+
+		const changes$ = search.valueChanges.pipe(takeUntilDestroyed(this.destroyRef));
+
+		changes$.subscribe(value => {
+			if (this.preventEmission) {
+				this.preventEmission = false;
+				return;
+			}
+
+			this.search.set(value);
+		});
 	}
 }
