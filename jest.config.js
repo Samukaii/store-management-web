5525a3be06c2f9b87b3a0f562005fc8eb6d83879
samukaii <samuelalejandro.dev@gmail.com>
2024-12-23 05:32:25 -0300

    refactor(#123): Configure Jest for code coverage
    - Added `jest-extended` package for extended matchers.
    - Added `collectCoverageFrom` property in `jest.config.js` to
      specify files for coverage collection.
    - Updated `test` script in `package.json` to include
      `--coverage` flag.
    - Added `global.d.ts` file to import `jest-extended`.
    - Added `files` array to `tsconfig.spec.json` to include
      `global.d.ts`.
    - Removed `jasmine` type from `tsconfig.app.json` types array.
    - Removed `@types/jasmine` devDependency from `package.json`.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
 	preset: 'jest-preset-angular',
 	setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+	collectCoverageFrom: [
+		'./src/app/shared/**/*.{ts,html}',
+		'./src/app/core/**/*.{ts,html}',
+		'./src/app/pages/**/*.{ts,html}',
+		'./src/app/app.component.{ts,html}',
+	],
 };
