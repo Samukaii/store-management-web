929c1e4fc6fc79419f73da0debfb28e1f938be37
samukaii <samuelalejandro.dev@gmail.com>
2024-12-23 05:32:43 -0300

    refactor: Improve tests setup
    - Added `jest-extended` matchers for better assertions.
    - Added a setup to clear current component fixture after
      each test to prevent unintended side effects.

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,3 +1,11 @@
 import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+import { clearCurrentComponentFixture } from "./src/app/testing/core/current-component-fixture";
+
+import * as matchers from 'jest-extended';
+expect.extend(matchers);
 
 setupZoneTestEnv();
+
+afterEach(() => {
+	clearCurrentComponentFixture();
+});
