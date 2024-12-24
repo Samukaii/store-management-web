ef33084d3298bb1941d3aefed9b79fae23d2b71f
samukaii <samuelalejandro.dev@gmail.com>
2024-12-24 18:17:59 -0300

    refactor(#123): Move DateOperationType to models and fix import
    - Moved `DateOperationType` from `src/app/shared/helpers`
      to `src/app/shared/models`.
    - Fixed the import path of `randomSchemaGenerators`
      in `random-schema-type.ts`.

--- src/app/shared/models/random-schema-type.ts
+++ src/app/shared/models/random-schema-type.ts
@@ -1,3 +1,3 @@
-import { randomSchemaGenerators } from "../helpers/random-schema-generators";
+import { randomSchemaGenerators } from "../helpers/random-schema-generators/random-schema-generators";
 
 export type RandomSchemaType = keyof typeof randomSchemaGenerators;
