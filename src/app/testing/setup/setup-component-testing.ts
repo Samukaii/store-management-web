480f985438aedc66e05154a221df697b86a0da54
samukaii <samuelalejandro.dev@gmail.com>
2024-12-24 18:19:41 -0300

    refactor(#123): Update component testing setup
    - Added `overrideImports` option to
      `SetupComponentTestingConfig`.
    - Updated overrideComponent to use
      `MetadataOverride` and conditionally override
      imports based on the `overrideImports` option.
    - Added imports from `@angular/core`.
    - Modified handling of imports for standalone
      components.

--- src/app/testing/setup/setup-component-testing.ts
+++ src/app/testing/setup/setup-component-testing.ts
@@ -1,9 +1,10 @@
-import { NO_ERRORS_SCHEMA, reflectComponentType, Type } from "@angular/core";
-import { TestBed, TestModuleMetadata } from "@angular/core/testing";
+import { Component, NO_ERRORS_SCHEMA, reflectComponentType, Type } from "@angular/core";
+import { MetadataOverride, TestBed, TestModuleMetadata } from "@angular/core/testing";
 import { setCurrentComponentFixture } from "../core/current-component-fixture";
 import { runBeforeComponentCreateActions } from "../core/before-component-create";
 
 interface SetupComponentTestingConfig extends TestModuleMetadata{
+	overrideImports?: boolean;
 }
 
 export const setupComponentTesting = <T>(component: Type<T>, config: SetupComponentTestingConfig) => {
@@ -12,12 +13,16 @@ export const setupComponentTesting = <T>(component: Type<T>, config: SetupCompon
 	const mirror = reflectComponentType(component);
 
 	if(mirror?.isStandalone) {
-		TestBed.overrideComponent(component, {
+		const componentOverride: MetadataOverride<Component> = {
 			set: {
-				schemas: [NO_ERRORS_SCHEMA],
-				imports: configCopy.imports ?? [],
-			},
-		})
+				schemas: [NO_ERRORS_SCHEMA]
+			}
+		};
+
+		if(config?.overrideImports !== false)
+			componentOverride.set!.imports = config?.imports ?? [];
+
+		TestBed.overrideComponent(component, componentOverride);
 
 		delete configCopy.imports;
 	}
