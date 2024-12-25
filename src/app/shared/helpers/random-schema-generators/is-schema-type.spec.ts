61695138c5ca45b7ea72123df2c5db69adb04081
samukaii <samuelalejandro.dev@gmail.com>
2024-12-25 15:58:36 -0300

    feat(#123): Add unit tests for isSchemaType helper
    - Added a new file named
      `is-schema-type.spec.ts` containing unit
      tests for the `isSchemaType` helper
      function.
    - Tests cover both valid and invalid
      inputs for the function.
    - Valid inputs include various
      `RandomSchemaType` values.
    - Invalid inputs include random strings,
      empty strings, objects, booleans,
      numbers, and dates.

---
+++ src/app/shared/helpers/random-schema-generators/is-schema-type.spec.ts
@@ -0,0 +1,44 @@
+import { isSchemaType } from "src/app/shared/helpers/random-schema-generators/is-schema-type";
+import { RandomSchemaType } from "src/app/shared/models/random-schema-type";
+
+describe(isSchemaType.name, () => {
+	const validCases: RandomSchemaType[] = [
+		"id",
+		"numeric",
+		"word:medium",
+		"word:long",
+		"word:short",
+		"phrase",
+		"text:short",
+		"text:medium",
+		"text:long",
+		"date",
+		"baseSelects",
+		"hour",
+		"trueFalse",
+	];
+
+	const invalidCases = [
+		"random-string",
+		"other-string",
+		"",
+		{},
+		true,
+		false,
+		0,
+		123,
+		new Date()
+	];
+
+	validCases.forEach(validCase => {
+		it(`must return true when receives "${validCase}"`, () => {
+			expect(isSchemaType(validCase)).toBe(true);
+		});
+	});
+
+	it(`must return false when receives invalid value`, () => {
+		invalidCases.forEach(invalidCase => {
+			expect(isSchemaType(invalidCase)).toBe(false);
+		})
+	});
+});
