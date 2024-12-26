8de44896638c9315075eb9f68ba0958fba442598
samukaii <samuelalejandro.dev@gmail.com>
2024-12-26 00:06:18 -0300

    feat(#123): Implement snackbar service
    - Created the SnackbarService to display snackbar messages.
    - Implemented the open method to handle snackbar options
      like level, message, action, and custom configurations.
    - Added unit tests to verify the service's behavior with
      different options and configurations.
    - Default action is "Fechar" and duration is 5000ms.
    - Allows overriding default action and config.

---
+++ src/app/shared/services/snackbar/snackbar.service.spec.ts
@@ -0,0 +1,88 @@
+import { TestBed } from "@angular/core/testing";
+import { SnackbarService } from "src/app/shared/services/snackbar/snackbar.service";
+import { SnackBarOptions } from "src/app/shared/services/models/snack-bar-options";
+import { MockProvider } from "ng-mocks";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { spyDependency } from "src/app/testing/spies/spy-dependency";
+
+interface SetupConfig {
+
+}
+
+const setup = (config?: SetupConfig) => {
+	TestBed.configureTestingModule({
+		providers: [MockProvider(MatSnackBar)],
+	})
+
+	const service = TestBed.inject(SnackbarService);
+
+	return {service};
+}
+
+fdescribe(SnackbarService.name, () => {
+	it('must instantiate service', () => {
+		const {service} = setup();
+
+		expect(service).toBeTruthy();
+	});
+
+	describe('When no config or action provided', () => {
+		const levels: SnackBarOptions['level'][] = [
+			'success',
+			'error'
+		];
+
+		levels.forEach(level => {
+			it(`must call snackbar open method adding panelClass "snackbar-${level}" with default action "Fechar" and duration as 5000 ms`, () => {
+				const {service} = setup();
+
+				const open = spyDependency(MatSnackBar, 'open');
+
+				service.open({
+					level,
+					message: "Some message"
+				});
+
+				expect(open).toHaveBeenCalledExactlyOnceWith("Some message", "Fechar", {
+					panelClass: `snackbar-${level}`,
+					duration: 5000,
+				});
+			});
+		})
+	});
+
+	describe('When config and action is provided', () => {
+		const levels: SnackBarOptions['level'][] = [
+			'success',
+			'error'
+		];
+
+		levels.forEach(level => {
+			it(`must call snackbar open method adding panelClass "snackbar-${level}" with action provided and options overriding default`, () => {
+				const {service} = setup();
+
+				const open = spyDependency(MatSnackBar, 'open');
+
+				service.open({
+					level,
+					message: "Some other message",
+					action: "Some action",
+					config: {
+						duration: 2331,
+						direction: "ltr",
+						verticalPosition: "top",
+						horizontalPosition: "center"
+					}
+				});
+
+				expect(open).toHaveBeenCalledExactlyOnceWith("Some other message", "Some action", {
+					panelClass: `snackbar-${level}`,
+					duration: 2331,
+					direction: "ltr",
+					verticalPosition: "top",
+					horizontalPosition: "center"
+				});
+			});
+		})
+	});
+});
--- src/app/shared/services/snackbar/snackbar.service.ts
+++ src/app/shared/services/snackbar/snackbar.service.ts
@@ -9,7 +9,7 @@ export class SnackbarService {
 	private snackbar = inject(MatSnackBar);
 
 	open(options: SnackBarOptions) {
-		this.snackbar.open(options.message, "Fechar", {
+		this.snackbar.open(options.message, options.action ?? "Fechar", {
 			panelClass: `snackbar-${options.level}`,
 			duration: 5000,
 			...options.config
