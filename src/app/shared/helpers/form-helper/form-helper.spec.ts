c88dd792c2a49d7e6d2ffc51975de36ce4bc3945
samukaii <samuelalejandro.dev@gmail.com>
2024-12-25 22:53:45 -0300

    feat(#0000): Add FormHelper and tests
    - Created FormHelper utility class.
    - Added `patchForm` method to update form controls with
      provided source values.
    - Implemented logic to apply modifiers to form control values
      during patching.
    - Added tests for `patchForm` method covering various scenarios:
      - Patching form controls with source values.
      - Applying a single modifier to a form control.
      - Handling cases where no modifier is found.
      - Applying multiple modifiers correctly.

---
+++ src/app/shared/helpers/form-helper/form-helper.spec.ts
@@ -0,0 +1,86 @@
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormHelper } from './form-helper';
+import { FormModifier } from "src/app/shared/models/form-modifier";
+
+describe('FormHelper', () => {
+	describe('patchForm', () => {
+		it('should patch form controls with source values', () => {
+			const form = new FormGroup({
+				name: new FormControl(''),
+				age: new FormControl(''),
+			});
+
+			const source = { name: 'John', age: 30 };
+			const modifiers: FormModifier<typeof form, typeof source>[] = [];
+
+			FormHelper.patchForm(form, source, modifiers);
+
+			expect(form.controls['name'].value).toBe('John');
+			expect(form.controls['age'].value).toBe(30);
+		});
+
+		it('should apply modifier to form control if modifier is found', () => {
+			const form = new FormGroup({
+				name: new FormControl(''),
+				age: new FormControl(''),
+			});
+
+			const source = { name: 'John', age: 30 };
+			const modifiers: FormModifier<typeof form, typeof source>[] = [
+				{
+					key: 'age',
+					modifier: (source: any) => source.age + 5,
+				},
+			];
+
+			FormHelper.patchForm(form, source, modifiers);
+
+			expect(form.controls['name'].value).toBe('John');
+			expect(form.controls['age'].value).toBe(35);
+		});
+
+		it('should not modify form control value if no modifier is found', () => {
+			const form = new FormGroup({
+				name: new FormControl(''),
+				age: new FormControl(''),
+			});
+
+			const source = { name: 'John', age: 30 };
+			const modifiers: FormModifier<FormGroup, typeof source>[] = [
+				{
+					key: 'address',
+					modifier: (source: any) => source.address,
+				},
+			];
+
+			FormHelper.patchForm(form, source, modifiers as any);
+
+			expect(form.controls['name'].value).toBe('John');
+			expect(form.controls['age'].value).toBe(30);
+		});
+
+		it('should apply multiple modifiers correctly', () => {
+			const form = new FormGroup({
+				name: new FormControl(''),
+				age: new FormControl(''),
+			});
+
+			const source = { name: 'John', age: 30 };
+			const modifiers: FormModifier<typeof form, typeof source>[] = [
+				{
+					key: 'name',
+					modifier: (source: any) => source.name.toUpperCase(),
+				},
+				{
+					key: 'age',
+					modifier: (source: any) => source.age + 5,
+				},
+			];
+
+			FormHelper.patchForm(form, source, modifiers);
+
+			expect(form.controls['name'].value).toBe('JOHN');
+			expect(form.controls['age'].value).toBe(35);
+		});
+	});
+});
