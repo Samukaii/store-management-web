9b5df5d495afffa4c84925b1ac837d421aa42b3e
samukaii <samuelalejandro.dev@gmail.com>
2024-12-28 19:51:41 -0300

    refactor(#123): Improve fixtureUtils typing
    - Added a new interface `ExtendedRootDebugElement` to
      correctly type the return of `fixtureUtils`.
    - The `fixtureUtils` function now returns an
      `ExtendedDebugElement` object.
    - The `changeInput` and `component` properties are now
      part of the returned `ExtendedDebugElement` object.
    - Removed unnecessary type casting.

--- src/app/testing/utils/fixture-utils.ts
+++ src/app/testing/utils/fixture-utils.ts
@@ -1,18 +1,25 @@
 import { getCurrentComponentFixture } from "src/app/testing/core/current-component-fixture";
-import { createExtendedDebugElement } from "src/app/testing/extenders/create-extended-debug-element";
 import { ComponentInputs } from "src/app/shared/models/component-inputs";
 import { ComponentInputValue } from "src/app/shared/models/component-input-value";
 import { Generic } from "src/app/shared/models/generic";
 import { changeInput } from "src/app/testing/core/change-input";
+import { ExtendedDebugElement } from "src/app/testing/models/extended-debug-element";
+import { createExtendedDebugElement } from "src/app/testing/extenders/create-extended-debug-element";
+
+interface ExtendedRootDebugElement<T extends Generic> extends ExtendedDebugElement<T> {
+	component: T,
+	changeInput: <K extends keyof ComponentInputs<T>>(name: K, value: ComponentInputValue<T, K>) => void;
+}
 
 export const fixtureUtils = <T extends Generic>() => {
 	const fixture = getCurrentComponentFixture<T>();
+	const debugElement = createExtendedDebugElement(fixture.debugElement) as ExtendedRootDebugElement<T>;
 
-	return {
-		component: fixture.componentInstance,
-		changeInput: <K extends keyof ComponentInputs<T>>(name: K, value: ComponentInputValue<T, K>) => {
-			changeInput(name as any, value);
-		},
-		...createExtendedDebugElement(fixture.debugElement)
-	}
+	debugElement.changeInput = (name, value) => {
+		changeInput<T>(name, value);
+	};
+
+	debugElement.component = fixture.componentInstance;
+
+	return debugElement;
 }
