9dea0ffc909e3990353efeca61aa4caaf6b68360
samukaii <samuelalejandro.dev@gmail.com>
2024-12-28 18:10:28 -0300

    refactor(#123): Add data-test-id to components
    - Added `data-test-id` to `mat-icon` in
      `no-results.component.html`.
    - Added `data-test-id` to `p` in
      `no-results.component.html`.
    - Created unit tests for
      `progress-spinner.component`.
    - Added `data-test-id` to `mat-progress-spinner`
      in `progress-spinner.component.html`.
    - Added `data-test-id` to `span` in
      `progress-spinner.component.html`.
    - Created unit tests for `no-results.component`.

---
+++ src/app/shared/components/no-results/no-results.component.spec.ts
@@ -0,0 +1,75 @@
+import { NoResultsComponent } from "src/app/shared/components/no-results/no-results.component";
+import { setupComponentTesting } from "src/app/testing/setup/setup-component-testing";
+import { hasCreatedComponent } from "src/app/testing/utils/has-created-component";
+import { getByTestId } from "src/app/testing/getters/get-by-test-id";
+import { fixtureUtils } from "src/app/testing/utils/fixture-utils";
+import { findByTestId } from "src/app/testing/getters/find-by-test-id";
+
+
+const setup = () => {
+	setupComponentTesting(NoResultsComponent, {
+		inputs: {
+			icon: "",
+			label: "",
+		}
+	});
+
+	return fixtureUtils<NoResultsComponent>();
+}
+
+fdescribe(NoResultsComponent.name, () => {
+	it('must create component', () => {
+		setup();
+
+		expect(hasCreatedComponent()).toBeTruthy();
+	});
+
+	it('must show icon provided by [icon] input', () => {
+		const {changeInput} = setup();
+
+		const icon = getByTestId('icon');
+
+		changeInput('icon', 'some-icon');
+		expect(icon.text()).toBe('some-icon');
+
+		changeInput('icon', 'other-icon');
+		expect(icon.text()).toBe('other-icon');
+	});
+
+	it('must show label provided by [label] input', () => {
+		const {changeInput} = setup();
+
+		const label = getByTestId('label');
+
+		changeInput('label', 'Some label');
+		expect(label.text()).toBe('Some label');
+
+		changeInput('label', 'Other label');
+		expect(label.text()).toBe('Other label');
+	});
+
+	describe('[description] input', () => {
+		it('must not show description if not provided', () => {
+			const {changeInput} = setup();
+
+			changeInput('description', undefined);
+
+			const description = findByTestId('description');
+
+			expect(description).toBeFalsy();
+		});
+
+		it('must show description with provided input', () => {
+			const {changeInput} = setup();
+
+			changeInput('description', 'Some description');
+
+			const description = getByTestId('description');
+
+			expect(description.text()).toBe('Some description');
+
+			changeInput('description', 'Other description');
+			expect(description.text()).toBe('Other description');
+		});
+	});
+});
---
+++ src/app/shared/components/progress-spinner/progress-spinner.component.spec.ts
@@ -0,0 +1,73 @@
+import { ProgressSpinnerComponent } from "src/app/shared/components/progress-spinner/progress-spinner.component";
+import { setupComponentTesting } from "src/app/testing/setup/setup-component-testing";
+import { hasCreatedComponent } from "src/app/testing/utils/has-created-component";
+import { fixtureUtils } from "src/app/testing/utils/fixture-utils";
+import { getByTestId } from "src/app/testing/getters/get-by-test-id";
+import { mockComponent } from "src/app/testing/mocks/mock-component";
+import { MatProgressSpinner } from "@angular/material/progress-spinner";
+import { findByTestId } from "src/app/testing/getters/find-by-test-id";
+
+
+const setup = () => {
+	setupComponentTesting(ProgressSpinnerComponent, {
+		imports: [
+			mockComponent(MatProgressSpinner)
+		]
+	});
+
+	return fixtureUtils<ProgressSpinnerComponent>();
+}
+
+fdescribe(ProgressSpinnerComponent.name, () => {
+	it('must create component', () => {
+		setup();
+
+		expect(hasCreatedComponent()).toBeTruthy();
+	});
+
+	it('must show a spinner with properties provided by input', () => {
+		const {changeInput} = setup();
+
+		const spinner = getByTestId('spinner');
+
+		expect(spinner.read(MatProgressSpinner)).toBeTruthy();
+
+		changeInput('mode', 'determinate');
+		expect(spinner.getProperty('mode')).toBe('determinate');
+
+		changeInput('mode', 'indeterminate');
+		expect(spinner.getProperty('mode')).toBe('indeterminate');
+
+		changeInput('strokeWidth', 5464);
+		expect(spinner.getProperty('strokeWidth')).toBe(5464);
+
+		changeInput('diameter', 6754);
+		expect(spinner.getProperty('diameter')).toBe(6754);
+	});
+
+	describe('[text] input', () => {
+		it('must not render text if not provided', () => {
+			const {changeInput} = setup();
+
+			changeInput('text', undefined);
+
+			const text = findByTestId('text');
+
+			expect(text).toBeFalsy();
+		});
+
+		it('must render input when provided with given text', () => {
+			const {changeInput} = setup();
+
+			changeInput('text', 'Some text');
+
+			const element = getByTestId('text');
+
+			expect(element.text()).toBe('Some text');
+
+			changeInput('text', 'Other text');
+
+			expect(element.text()).toBe('Other text');
+		});
+	});
+});
