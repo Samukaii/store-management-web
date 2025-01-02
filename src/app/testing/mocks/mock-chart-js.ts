3d913269268088f48ba660486142eb6d24d8ab95
samukaii <samuelalejandro.dev@gmail.com>
2025-01-02 19:50:51 -0300

    refactor(#1234): Add mock for chartjs-plugin-datalabels
    - Added a mock for the `chartjs-plugin-datalabels` plugin
      in the `mock-chart-js.ts` file.
    - Added `dataLabel` to the return of `getMockedChartJs`
      function in `get-mocked-chart-js.ts` file.

--- src/app/testing/mocks/mock-chart-js.ts
+++ src/app/testing/mocks/mock-chart-js.ts
@@ -4,4 +4,6 @@ export const mockChartJs = () => {
 	jest.mock('chart.js/auto', () => {
 		return getMockedChartJs().classMock;
 	});
+
+	jest.mock('chartjs-plugin-datalabels', () => getMockedChartJs().plugins.dataLabel);
 }
--- src/app/testing/utils/get-mocked-chart-js.ts
+++ src/app/testing/utils/get-mocked-chart-js.ts
@@ -2,8 +2,12 @@ const instance = {
 	destroy: jest.fn(),
 }
 
+const dataLabel = {
+	plugin: "data-label",
+}
+
 const classMock = jest.fn().mockImplementation(() => instance);
 
 export const getMockedChartJs = () => {
-	return {classMock, instance};
+	return {classMock, instance, plugins: {dataLabel}};
 };
